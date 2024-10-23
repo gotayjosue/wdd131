dd3be8a5f6ad24a5165a4f94ba94a6af13681e37
gotayjosue <gotayjosue@gmail.com>
2024-10-22 22:43:15 -0600

    search function added

--- project/scripts/home.js
+++ project/scripts/home.js
@@ -7,3 +7,55 @@ document.addEventListener("DOMContentLoaded", function() {
         navigation.classList.toggle("menu-open");
     });
 });
+
+let photosList = []
+
+const gallery = document.getElementById('gallery');
+const search = document.getElementById('search-bar');
+const noResultsMessage = document.querySelector('#noResultsMessage')
+
+function displayPhotos(photos){
+    gallery.innerHTML = ''
+
+    if (photos.length === 0){
+        noResultsMessage.style.display = 'block';
+    }
+    else{
+        photos.forEach(photo => {
+            noResultsMessage.style.display = 'none'
+            const image = document.createElement('img')
+            image.src = photo.imageUrl
+            image.alt = photo.description
+            image.loading = "lazy"
+
+            gallery.append(image)
+
+        });
+    }
+}
+
+const getPhotos = async() => {
+
+    const response = await fetch("https://gotayjosue.github.io/wdd131/project/photos.json")
+
+    photosList = await response.json();
+
+    displayPhotos(photosList);
+}
+
+
+function searchPhotos(query){
+    const filteredPhotos = photosList.filter(photo => photo.description.toLowerCase().includes(query.toLowerCase()))
+    displayPhotos(filteredPhotos)
+}
+
+search.addEventListener('keypress', (event) => {
+
+    if (event.key === 'Enter'){
+        const searchTerm = search.value.trim();
+        searchPhotos(searchTerm)
+    }
+
+});
+
+getPhotos()
