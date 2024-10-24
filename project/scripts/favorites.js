0e7aa72d5f3da3d89bfb92f9048a64a58d9feb5f
gotayjosue <gotayjosue@gmail.com>
2024-10-23 22:51:41 -0600

    favorites page fixed

--- project/scripts/favorites.js
+++ project/scripts/favorites.js
@@ -18,10 +18,36 @@ if (favoritePhotos.length > 0) { /*Iterating through the favorites array to put 
     favoritePhotos.forEach(photo => {
 
         const image = document.createElement('img');
+
+        const photoContainer = document.createElement('div')
+        photoContainer.classList.add('photoContainer')
+
+        const overlay = document.createElement('div')
+        overlay.classList.add('overlay')
+
+        const eraseButton = document.createElement('button')
+        eraseButton.classList.add('erase')
+        eraseButton.textContent = 'Delete From Favorites'
+
+        const star = document.createElement('span')
+        star.classList.add('star')
+        star.textContent = '🌟'
+
         image.src = photo.imageUrl;
         image.alt = photo.description
 
-        favoriteContainer.appendChild(image)
+        photoContainer.appendChild(star)
+        photoContainer.appendChild(image)
+        photoContainer.appendChild(eraseButton)
+        favoriteContainer.appendChild(photoContainer)
+        
+        eraseButton.addEventListener('click', () =>{
+
+            const updateFavorites = favoritePhotos.filter(fav => fav.imageUrl !== photo.imageUrl)
+            localStorage.setItem('favorites', JSON.stringify(updateFavorites));
+
+            photoContainer.remove()
+        })
         
     });
 }
